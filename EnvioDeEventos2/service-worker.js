110256141e0f03aa617fa703a9b958d2140f6751
CeaarDaniel <73078916+CeaarDaniel@users.noreply.github.com>
2024-09-09 14:34:19 -0600

    Add files via upload

---
+++ EnvioDeEventos2/service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installed');
+  });
+  
+  self.addEventListener('activate', (event) => {
+    console.log('Service Worker activated');
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
